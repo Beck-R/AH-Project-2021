3c98d043fb0efcf32872955a26f24f3ea16ec329
Jason Antwi-Appah <jasonantwiappah@gmail.com>
2021-02-27 22:46:20 +0000

    add basic error handling to flask server

--- app.py
+++ app.py
@@ -6,17 +6,27 @@ dictData = {}
 
 @app.route('/api/computers/<id>/getData', methods=['POST'])
 def getData(id):
-    data = request.json
-    dictData[id] = data
-    return "Success"
+    try:
+        data = request.json
+        dictData[id] = data
+        return "Success"
+    except Exception as e:
+        return formatErr(e)
 
 @app.route('/api/computers/<id>/sendData',  methods=['GET', 'POST'])
 def send_data(id):
-    if id in dictData:
-        data = dictData[id]
-        return jsonify(data)
-    else:
-        return "Node not found"
+    try:
+        if id in dictData:
+            data = dictData[id]
+            return jsonify(data)
+        else:
+            return "Node not found"
+    except Exception as e:
+        return formatErr(e)
+
+def formatErr(e): 
+    return f"Error: \n\n {e}"
+
 
 @app.route('/')
 def index():
