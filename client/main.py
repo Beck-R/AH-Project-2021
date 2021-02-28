ee2ffbc7b2b90e16e914688f69f40ee488d557f8
glitchy-impostor <75087610+glitchy-impostor@users.noreply.github.com>
2021-03-01 02:02:57 +0530

    Update main.py

--- client/main.py
+++ client/main.py
@@ -1,5 +1,13 @@
 from getinfo import sys_monitor
 from getprocess import proc_monitor
+from server import main
+import requests
+
+host = socket.gethostbyname(socket.gethostname())
+port = random.randint(10000, 10100)
+r = requests.post(f'http://192.168.1.100:8080/api/computers/{platform.uname().node}/ipPort', json={'ip': host, 'port': port})
+main(host=host, port=port)
+
 
 sys_monitor()
 proc_monitor()
