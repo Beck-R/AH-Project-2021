7867242459e7a6e0c85e955e15cac703717db846
Jason Antwi-Appah <jasonantwiappah@gmail.com>
2021-02-28 23:26:18 +0000

    make flask server publically accessible

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',port=8080, threaded=True)
+    app.run(host='0.0.0.0',port=8080, threaded=True)
